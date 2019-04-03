3e2b31a210944da42c59b230835b31d6fa210e78
JPMormon <49249604+JPMormon@users.noreply.github.com>
2019-04-03 21:28:03 +0300

    Up
    Attempt #2

--- Corrected.php
+++ Corrected.php
@@ -1,37 +1,32 @@
 <?php
 
-/**
- * Some namespace, some libraries, some annotations.
- */
 class ShiftCenterRestResource extends ResourceBase {
 
-  /**
-   * Some code.
-   */
-
-  /**
-   * Get if user has access to this shift center.
-   *
-   * @param integer $uid
-   *   User id.
-   * @param integer $id
-   *   Shift center id.
-   *
-   * @return bool
-   *   True if user has access.
-   */
   private function getAccess($uid, $id) {
-    /**
-     * Some code.
-     */
-
-    $access = TRUE;
-
-    return $access;
-
+    $variable = 'shiftcenter_access';
+    $variable .= '_' . (string) $uid;
+    $variable .= '_' . (string) $id;
+    $access = &drupal_static($variable);
+	    if (isset($access)) {
+		    return $access;
+		}
+	    if ($cache = $this->cache->get($variable)) {
+	        return $cache->data;
+        } 
+        $shift_access = $this->shiftAccess;
+		if (!$shift_access->getPositionSameGroup($uid, $id)) {
+			return $this->duplicateExterminator(false,$variable);
+		}
+		$permission = $shift_access->getActionPermission($uid, 'action_shifts_view_bellow') || $shift_access->getActionPermission($uid, 'action_shifts_edit_bellow');
+        $below = $shift_access->getPositionBelow($uid, $id);
+        if ($shift_access->getActionPermission($uid, 'action_shifts_edit_all') || $shift_access->getActionPermission($uid, 'action_shifts_view_all') || ($permission && $below)) {
+            return $this->duplicateExterminator(true,$variable);
+        } 
+        return $this->duplicateExterminator(false,$variable);
   }
-
-  /**
-   * Some code.
-   */
+  private function duplicateExterminator($bool,$variable) {
+        $this->cache->set($variable, $bool, $this->shiftTime->getTimestamp() + SHIFT_ACCESS_CACHE_TIME);
+        return $bool;
+  }
 }
+
