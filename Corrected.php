4cdc155b92adf4c95e423fd56df27672130609db
JPMormon <49249604+JPMormon@users.noreply.github.com>
2019-04-08 02:56:00 +0300

    Update Corrected.php

--- Corrected.php
+++ Corrected.php
@@ -7,23 +7,28 @@ class ShiftCenterRestResource extends ResourceBase {
     $variable .= '_' . (string) $uid;
     $variable .= '_' . (string) $id;
     $access = &drupal_static($variable);
-	    if (isset($access)) {
-		    return $access;
-		}
-	    if ($cache = $this->cache->get($variable)) {
-	        return $cache->data;
-        } 
-        $shift_access = $this->shiftAccess;
-		if (!$shift_access->getPositionSameGroup($uid, $id)) {
-			return $this->duplicateExterminator(false,$variable);
-		}
-		$permission = $shift_access->getActionPermission($uid, 'action_shifts_view_bellow') || $shift_access->getActionPermission($uid, 'action_shifts_edit_bellow');
-        $below = $shift_access->getPositionBelow($uid, $id);
-        if ($shift_access->getActionPermission($uid, 'action_shifts_edit_all') || $shift_access->getActionPermission($uid, 'action_shifts_view_all') || ($permission && $below)) {
+    if (isset($access)) {
+	return $access;
+    }
+	  
+    if ($cache = $this->cache->get($variable)) {
+        return $cache->data;
+    } 
+	  
+    $shift_access = $this->shiftAccess;
+    if (!$shift_access->getPositionSameGroup($uid, $id)) {
+	return $this->duplicateExterminator(false,$variable);
+    }
+
+    if ($shift_access->getActionPermission($uid, 'action_shifts_edit_all') 
+        || $shift_access->getActionPermission($uid, 'action_shifts_view_all') 
+        || (($shift_access->getActionPermission($uid, 'action_shifts_view_bellow') || $shift_access->getActionPermission($uid, 'action_shifts_edit_bellow')) 
+            && $shift_access->getPositionBelow($uid, $id);)) {
             return $this->duplicateExterminator(true,$variable);
-        } 
-        return $this->duplicateExterminator(false,$variable);
+    } 
+    return $this->duplicateExterminator(false,$variable);
   }
+	
   private function duplicateExterminator($bool,$variable) {
         $this->cache->set($variable, $bool, $this->shiftTime->getTimestamp() + SHIFT_ACCESS_CACHE_TIME);
         return $bool;
